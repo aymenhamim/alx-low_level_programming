d7f9d3264fe82525a4500d729c6f0a693a8b1fc2
Aymen hamim <aymanjhamim@gmail.com>
2023-07-17 11:49:12 -0700

    low level

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>*
+/**
+ * main - entry point
+ *
+ * Description: A C program thath prints with put function
+ *
+ * Return: always 0 (Succes)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
