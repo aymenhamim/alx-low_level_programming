c25c7e692174e17e789052f0bc5ed9914645dd57
Aymen hamim <aymanjhamim@gmail.com>
2023-07-21 12:10:24 -0700

    low level

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using the character #
+ *
+ * @size: is the size of the square
+ *
+ * Return: Always 0 (Success)
+*/
+void print_square(int size)
+{
+	int row, colum;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (colum = 1; colum <= size; colum++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
+
