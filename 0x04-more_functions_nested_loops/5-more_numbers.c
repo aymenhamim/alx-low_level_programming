168cfb8158c14f88a2739bdf30a23e019a7bcc78
Aymen hamim <aymanjhamim@gmail.com>
2023-07-21 08:47:12 -0700

    low level

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,18 @@ void more_numbers(void)
 {
 	int num, row, count;
 
-	for (row = 1; row <= 14; count++)
+	for (row = 1; row <= 10; row++)
 	{
-		num = count;
-		if (count > 9)
+		for (row = 1; row <= 14; count++)
 		{
-			_putchar(1 + 48)
-			num = count % 10;
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
-		_putchar(num + 48);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
