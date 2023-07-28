aa6301896126de68de1b56f586c7054e7ddbce08
Aymen hamim <aymanjhamim@gmail.com>
2023-07-28 15:54:52 -0700

    low level

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,26 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
- */
-
-void print_number(int n)
+int main(void)
 {
-	unsigned int n1;
+	int n;
+	int a[5];
+	int *p;
 
-	n1 = n;
+	a[2] = 1024;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n;
-	}
-
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	p = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
+
