c0244e6970a0fd396e582a6cda20fb01e54e5626
Aymen hamim <aymanjhamim@gmail.com>
2023-08-09 16:52:02 -0700

    0x0A

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,11 +3,12 @@
 #include "main.h"
 
 /**
- * main-it all starts here
+ * main - it  all starts here
  * @argc: the number of arguments
- * @argv: array of pointers to argument
+ * @argv: array of pointers to arguments
+ *
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
 	int sum = 0;
@@ -18,8 +19,9 @@ int main(int argc, char *argv[])
 		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
-		sum += atio(argv[argc]);
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
+	return (0);
 }
 
