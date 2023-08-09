d91964024ad6c546ee8593155ac6686053ce0529
Aymen hamim <aymanjhamim@gmail.com>
2023-08-09 14:51:29 -0700

    0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program namee, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Retun: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[]);
+	return (0);
+}
